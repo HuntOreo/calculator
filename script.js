446b93373d48cb19b5ea6be1b05c988aaded243b
HuntOreo <hloreair@outlook.com>
2025-07-17 01:17:24 -0400

    Build calculator logic

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function calculate(intOne, intTwo, operator) {
   try {
     errorHandler(intOne, intTwo, operator);
+
+    switch (operator) {
+      case '+':
+        return add(intOne, intTwo);
+      case '-':
+        return subtract(intOne, intTwo);
+      case '*':
+        return multiply(intOne, intTwo);
+      case '/':
+        return divide(intOne, intTwo);
+    }
+
   } catch (err) {
     alert(err.message);
   }
@@ -38,5 +50,9 @@ function errorHandler(intOne, intTwo, operator) {
   if (typeof intOne != 'number' || typeof intTwo != 'number') {
     throw invalidTypeErr;
   }
-
 }
+
+console.log('add: ' + calculate(1, 2, '+'));
+console.log('subtract: ' + calculate(5, 3, '-'));
+console.log('multiply: ' + calculate(7, 2, '*'));
+console.log('divide: ' + calculate(6, 2, '/'));
