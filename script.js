fb6a7cfb46eb5f26607e00751ffa38abb7e6ec65
HuntOreo <hloreair@outlook.com>
2025-07-17 00:57:42 -0400

    Create basic operation functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+function calculate(intOne, intTwo, operator) {
+  console.log(intOne, intTwo, operator);
+}
+
+function add(intOne, intTwo) {
+  return intOne + intTwo;
+}
+
+function subtract(intOne, intTwo) {
+  return intOne - intTwo;
+}
+
+function multiply(intOne, intTwo) {
+  return intOne * intTwo;
+}
+
+function divide(intOne, intTwo) {
+  return intOne / intTwo;
+}
